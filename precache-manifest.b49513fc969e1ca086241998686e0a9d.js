9e3a5b5b413ae2e39b764fd29a4a2f13d00c0726
lshaw212 <lshaw212@gmail.com>
2020-11-24 21:55:50 +0000

    Updates

---
+++ precache-manifest.b49513fc969e1ca086241998686e0a9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e9bc7c0d0402a6c77a334899bc691e",
+    "url": "/wordsearch-gen/index.html"
+  },
+  {
+    "revision": "a8986633834023f15869",
+    "url": "/wordsearch-gen/static/css/main.ba73fd95.chunk.css"
+  },
+  {
+    "revision": "3e9966c5de7670308568",
+    "url": "/wordsearch-gen/static/js/2.02c10c87.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/wordsearch-gen/static/js/2.02c10c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8986633834023f15869",
+    "url": "/wordsearch-gen/static/js/main.0df8068a.chunk.js"
+  },
+  {
+    "revision": "9070c500b754e823bfc2",
+    "url": "/wordsearch-gen/static/js/runtime-main.e6edcfcb.js"
+  }
+]);
